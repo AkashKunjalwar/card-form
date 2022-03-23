3d57bf41aacb44b1c542abee65379d3bb70d25ff
Akash <kunjalwar.akash@gmail.com>
2022-03-23 22:46:28 +0530

    Added from Second Clone

--- src/Components/OverlappingCard.js
+++ src/Components/OverlappingCard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const OverlappingCard = () => {
-  return <h1>OverlappingCard</h1>;
+  return (
+    <div className="overlapping-card">
+      <div className="top-section">
+        <div className="silver square box"></div>
+        <div className="card-type"></div>
+      </div>
+      <div className="middle-section">
+        <div className="16-digit-card-number"></div>
+      </div>
+    </div>
+  );
 };
 
 export default OverlappingCard;
