a23c9e0dde05416f5ba1133b0fe58108858ecefc
Akash <kunjalwar.akash@gmail.com>
2022-03-23 22:28:45 +0530

    Added JSX code in Branch

--- src/Components/CreditCardForm.js
+++ src/Components/CreditCardForm.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const CreditCardForm = () => {
-  return <h1>CreditCardForm</h1>;
+  return (
+    <div>
+      <form>
+        <h1>CreditCardForm</h1>
+        <input type="text" placeholder="Enter the card number" />
+        <input type="text" placeholder="Enter the card holder name" />
+        <input type="text" placeholder="Enter the expiry month/year" />
+        <input type="password" placeholder="Enter CVV" />
+      </form>
+    </div>
+  );
 };
 
 export default CreditCardForm;
