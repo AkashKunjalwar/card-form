cfba7878cf24d0ec83087385e132620eb739622b
Akash <kunjalwar.akash@gmail.com>
2022-03-23 22:47:54 +0530

    Changes from First

--- src/Components/CreditCardForm.js
+++ src/Components/CreditCardForm.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 const CreditCardForm = () => {
+  const submitCardHandler = (e) => {
+    console.log("submitted");
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitCardHandler}>
         <h1>CreditCardForm</h1>
         <input type="text" placeholder="Enter the card number" />
         <input type="text" placeholder="Enter the card holder name" />
         <input type="text" placeholder="Enter the expiry month/year" />
         <input type="password" placeholder="Enter CVV" />
+        <button>Submit</button>
       </form>
     </div>
   );
